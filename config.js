817ff4c5d5840172c1149b9783d1bdf1dd3bd06b
HassanKhan691 <mexonley@gmail.com>
2024-07-21 11:26:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -30,12 +30,12 @@ global.prefa = process.env.PREFIX ? process.env.PREFIX.split(",") : ["."];
 
 
 // Owner information
-global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["916297175943", "919362639362"];
-global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["916297175943", "919362639362"];
-global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["916297175943"];
+global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["923216520225", "923216520225"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["923216520225", "923216520225"];
+global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["923216520225"];
 global.OwnerName = process.env.OWNER_NAME || "Kai";
-global.BotName = process.env.BOT_NAME || "A17";
-global.packname = process.env.PACK_NAME || "A17 Bot";
+global.BotName = process.env.BOT_NAME || "EAGLE";
+global.packname = process.env.PACK_NAME || "BLACK_EAGLE";
 global.author = "By: Kai";
 global.BotSourceCode = "https://github.com/Kai0071/A17";
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
